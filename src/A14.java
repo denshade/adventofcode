b3fff6f851da57ebcaad4603f95b82113aff79fb
denshade <lieven.vaneeckhaute@gmail.com>
2022-12-17 17:16:40 +0100

    trickle

--- src/A14.java
+++ src/A14.java
@@ -4,17 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class A14
-{
-    public static List<List<Point>> parseBoard(File file) throws IOException {
+public class A14 {
+    public static List<List<Point>> parsePoints(File file) throws IOException {
         List<String> lines = Files.readAllLines(file.toPath());
         var points = new ArrayList<List<Point>>();
-        for(String line: lines)
-        {
-            var lineOfPoints  = new ArrayList<Point>();
+        for (String line : lines) {
+            var lineOfPoints = new ArrayList<Point>();
             var pointParts = line.split(" -> ");
-            for (String pointPart: pointParts) {
+            for (String pointPart : pointParts) {
                 var miniparts = pointPart.split(",");
                 Point p = new Point(Integer.parseInt(miniparts[0]), Integer.parseInt(miniparts[1]));
                 lineOfPoints.add(p);
@@ -22,5 +21,87 @@ public class A14
             points.add(lineOfPoints);
         }
         return points;
+    }
+
+    public static char[][] findFinalPosition(char[][] board, Point particlePosition)
+    {
+        // while can trickleDown, do
+        // if no longer can trickleDown
+        // mark on the board.
+        return board;
+    }
+
+    public static Point trickleDown(char[][] board, Point particlePosition)
+    {
+        /*
+        A unit of sand always falls down one step if possible.
+        If the tile immediately below is blocked (by rock or sand), the unit of sand attempts to instead move
+         diagonally one step down and to the left. If that tile is blocked, the unit of sand attempts to instead move
+          diagonally one step down and to the right. Sand keeps moving as long as it is able to do so, at each step
+          trying to move down, then down-left, then down-right. If all three possible destinations are blocked,
+          the unit of sand comes to rest and no longer moves,
+          at which point the next unit of sand is created back at the source.
+         */
+        return particlePosition;
+    }
+
+    public static String showBoard(int fromX, int toX, int fromY, int toY, char[][] board) {
+        var builder = new StringBuilder();
+        for (int y = fromY; y <= toY; y++)
+        {
+            for (int x = fromX; x <= toX; x++) {
+                if (board[y][x] == 0) {
+                    builder.append(' ');
+                } else {
+                    builder.append(board[y][x]);
+                }
+            }
+            builder.append('\n');
+        }
+        return builder.toString();
+    }
+
+    public static char[][] parseBoard(File file) throws IOException {
+        char[][] board = buildEmptyBoard(file);
+
+        for (List<Point> rowOfPoints : parsePoints(file)) {
+            var currentPoint = rowOfPoints.get(0);
+            for (int pointIndex = 1; pointIndex < rowOfPoints.size(); pointIndex++) {
+                markPoints(rowOfPoints.get(pointIndex),currentPoint, board);
+                currentPoint = rowOfPoints.get(pointIndex);
+            }
+        }
+        return board;
+    }
+
+    public static char[][] markPoints(Point fromPoint, Point toPoint, char[][] board) {
+        for (int x = fromPoint.x; x <= toPoint.x; x++) {
+            board[fromPoint.y][x] = '#';
+        }
+        for (int x = toPoint.x; x <= fromPoint.x; x++) {
+            board[toPoint.y][x] = '#';
+        }
+        for (int y = fromPoint.y; y <= toPoint.y; y++) {
+            board[y][fromPoint.x] = '#';
+        }
+        for (int y = toPoint.y; y <= fromPoint.y; y++) {
+            board[y][toPoint.x] = '#';
+        }
+        return board;
+    }
+
+    private static char[][] buildEmptyBoard(File file) throws IOException {
+        var maxX = 0;
+        var maxY = 0;
+        for (List<Point> rowOfPoints : parsePoints(file)) {
+            if (rowOfPoints.stream().mapToInt(e -> e.x).max().orElse(0) > maxX) {
+                maxX = rowOfPoints.stream().mapToInt(e -> e.x).max().orElse(0);
+            }
+            if (rowOfPoints.stream().mapToInt(e -> e.y).max().orElse(0) > maxY) {
+                maxY = rowOfPoints.stream().mapToInt(e -> e.y).max().orElse(0);
+            }
+        }
+        char[][] board = new char[maxY + 1][maxX + 1];
+        return board;
     }
 }
--- src/A14Test.java
+++ src/A14Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,10 +10,30 @@ class A14Test
 {
 
     @Test
-    void parseBoard() throws IOException {
+    void parsePoints() throws IOException {
+        var points= A14.parsePoints(new File("a14.test.txt"));
+        assertEquals(2, points.size());
+        assertEquals(3, points.get(0).size());
+        assertEquals(4, points.get(1).size());
+    }
+
+    @Test
+    void createBoard() throws IOException {
         var board= A14.parseBoard(new File("a14.test.txt"));
-        assertEquals(2, board.size());
-        assertEquals(3, board.get(0).size());
-        assertEquals(4, board.get(1).size());
+        assertEquals("", A14.showBoard(494, 503, 0, 9, board));
+    }
+
+    @Test
+    void markPositions() throws IOException {
+        var board = new char[5][5];
+        A14.markPoints(new Point(1,0), new Point(1,1), board);
+        assertEquals('#', board[0][1] );
+        assertEquals('#', board[1][1] );
+    }
+
+    @Test
+    void trickleBoard() throws IOException {
+        var board= A14.parseBoard(new File("a14.test.txt"));
+        assertEquals(new Point(500,1), A14.trickleDown(board, new Point(500,0)));
     }
 }
