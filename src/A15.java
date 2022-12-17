0ec3fd063cf77376e4eefc47b1e26eb8b2ffd30c
Lieven <lieven.vaneeckhaute@gmail.com>
2022-12-17 22:26:15 +0100

    A15.

--- src/A15.java
+++ src/A15.java
@@ -8,22 +8,65 @@ import java.util.List;
 public class A15
 {
     static class Sensor {
+
+        public Sensor(Point location, Point closestBeacon)
+        {
+            this.location = location;
+            this.closestBeacon = closestBeacon;
+            distanceToBeacon = calculate(location, closestBeacon);
+        }
         Point location;
         Point closestBeacon;
+        long distanceToBeacon;
+
+        public boolean isInRange(Point position)
+        {
+            return calculate(location, position) <= distanceToBeacon;
+        }
     }
+    public static long calculate(Point p1, Point p2) {
+        int dist = Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y);
+        return dist;
+    }
+
+    public static long calculateOnLine(int y, List<Sensor> sensors)
+    {
+        int correct = 0;
+        for (int x = -10000000; x < 10000000; x++) {
+            correct = getCorrect(y, sensors, correct, x);
+        }
+        return correct;
+    }
+
+    private static int getCorrect(int y, List<Sensor> sensors, int correct, int x) {
+        for (Sensor sensor : sensors) {
+            if (sensor.isInRange(new Point(x, y))) {
+                correct++;
+                break;
+            }
+        }
+        for (Sensor sensor : sensors) {
+            if (sensor.closestBeacon.equals(new Point(x, y))) {
+                correct--;
+                break;
+            }
+        }
+
+        return correct;
+    }
+
     static List<Sensor> loadSensors(File file) throws IOException {
         List<String> lines = Files.readAllLines(file.toPath());
         var lst = new ArrayList<Sensor>();
         for (String l : lines) {
-            var sensor = new Sensor();
             var sensorL = l.split(":")[0];
             var rX = sensorL.substring("Sensor at x=".length(), sensorL.indexOf(','));
             var rY = sensorL.substring(sensorL.indexOf(',') + 4);
             var beaconL = l.split(":")[1];
             var bX = beaconL.substring(" closest beacon is at x=".length(), beaconL.indexOf(','));
             var bY = beaconL.substring(beaconL.indexOf(',') + 4);
-            sensor.location = new Point(Integer.parseInt(rX), Integer.parseInt(rY));
-            sensor.closestBeacon = new Point(Integer.parseInt(bX), Integer.parseInt(bY));
+            var sensor = new Sensor(new Point(Integer.parseInt(rX), Integer.parseInt(rY)),
+                    new Point(Integer.parseInt(bX), Integer.parseInt(bY)));
             lst.add(sensor);
         }
         return lst;
--- src/A15Test.java
+++ src/A15Test.java
@@ -11,5 +11,14 @@ class A15Test
     void loadSensors() throws IOException {
         assertEquals(14, A15.loadSensors(new File("a15.test.txt")).size());
     }
+    @Test
+    void loadData() throws IOException {
+        assertEquals(26, A15.calculateOnLine(10, A15.loadSensors(new File("a15.test.txt"))));
+    }
+
+    @Test
+    void loadDataFull() throws IOException {
+        assertEquals(26, A15.calculateOnLine(2000000, A15.loadSensors(new File("a15.full.txt"))));
+    }
 
 }
