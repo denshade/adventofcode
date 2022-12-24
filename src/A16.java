ad9072b1ab4b56256034decbbfcd547312e4ae78
denshade <lieven.vaneeckhaute@gmail.com>
2022-12-24 21:03:49 +0100

    A16 brute.

--- src/A16.java
+++ src/A16.java
@@ -1,18 +1,93 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class A16
 {
     public static class Valve {
         String name;
         int rate;
+        boolean open = false;
         List<Valve> valves = new ArrayList<>();
     }
+
+    public static ValveSystem calculate(List<Valve> valves)
+    {
+        long bestSolutionScore = 0;
+
+        var sys = new ValveSystem(valves.get(0), valves);
+        PriorityQueue<ValveSystem> systems = new PriorityQueue<>();
+        systems.add(sys);
+        while(systems.size() > 0) {
+            var currentSys = systems.poll();
+            if (currentSys.nrTicks == 30) {
+                if (currentSys.totalRate > bestSolutionScore) {
+                    bestSolutionScore = currentSys.totalRate;
+                    System.out.println(bestSolutionScore);
+                }
+            }
+            if (!currentSys.currentValve.open) {
+                systems.add(currentSys.openValve().tick());
+            }
+            for (var availableValves : currentSys.currentValve.valves) {
+                systems.add(currentSys.gotoValve(availableValves).tick());
+            }
+        }
+        return sys;
+    }
+
+    public static class ValveSystem implements Comparable<ValveSystem>
+    {
+        public Valve currentValve;
+        public List<Valve> allValves;
+        public long totalRate = 0;
+        public long nrTicks = 0;
+
+        public ValveSystem(Valve currentValve, List<Valve> otherValves) {
+            this.currentValve = currentValve;
+            this.allValves = otherValves;
+        }
+        public ValveSystem(ValveSystem system) {
+            this.currentValve = system.currentValve;
+            this.allValves = system.allValves;
+            this.totalRate = system.totalRate;
+            this.nrTicks = system.nrTicks;
+        }
+
+        public ValveSystem tick()
+        {
+            var newValveSystem = new ValveSystem(this);
+            newValveSystem.nrTicks++;
+            newValveSystem.totalRate += newValveSystem.allValves.stream().filter(v -> v.open).mapToLong(v -> v.rate).sum();
+            return newValveSystem;
+        }
+
+
+        public ValveSystem gotoValve(Valve valve)
+        {
+            var newValveSystem = new ValveSystem(this);
+            newValveSystem.currentValve = valve;
+            return newValveSystem;
+        }
+
+        public ValveSystem openValve()
+        {
+            var newValveSystem = new ValveSystem(this);
+            newValveSystem.currentValve.open = true;
+            return newValveSystem;
+        }
+
+        @Override
+        public int compareTo(ValveSystem o) {
+            return (int)(-1*this.totalRate) - (int)(-1*o.totalRate);
+        }
+    }
+    public static long calculate(File file) throws IOException {
+        var valves = loadValves(file);
+        return calculate(valves).totalRate;
+    }
+
     public static List<Valve> loadValves(File file) throws IOException {
         List<String> lines = Files.readAllLines(file.toPath());
         List<Valve> valves = new ArrayList<>();
--- src/A16Test.java
+++ src/A16Test.java
@@ -10,5 +10,23 @@ class A16Test {
     void loadVals() throws IOException {
         assertEquals(10, A16.loadValves(new File("a16.test.txt")).size());
     }
+    @Test
+    void checkSolution() throws IOException {
+        assertEquals(1651, A16.loadValves(new File("a16.test.txt")).size());
+    }
+
+    @Test
+    void checkSolutionSys() throws IOException {
+        assertEquals(1651, A16.calculate(A16.loadValves(new File("a16.test.txt"))).totalRate);
+    }
+
+    @Test
+    void checkValveOpened() throws IOException {
+        var valves = A16.loadValves(new File("a16.test.txt"));
+        var v = new A16.ValveSystem(valves.get(0), valves).gotoValve(valves.get(1)).openValve().tick();
+        assertEquals(1, v.nrTicks);
+        assertEquals(13, v.totalRate);
+
+    }
 
 }
