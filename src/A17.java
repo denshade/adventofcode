19d5f4563cb390c3886efde1649fd5d0d9a754da
Lieven <lieven.vaneeckhaute@gmail.com>
2022-12-29 22:13:13 +0100

    First start for A17.

--- src/A17.java
+++ src/A17.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class A17
@@ -10,8 +11,22 @@ public class A17
             return moves.chars().mapToObj(e-> e=='<'?Left:Right).collect(Collectors.toList());
         }
     }
+
+    public enum Block {
+        Minus, Plus, L, I, Block
+    }
+
+    public static class Board {
+        Board dropBlock(Block block, Queue<Moves> nextMoves)
+        {
+            return this;
+        }
+    }
     public static int calculateHeight(String directions)
     {
+        for (int block = 0; block < 2022; block++) {
+
+        }
         return 0;
     }
 
